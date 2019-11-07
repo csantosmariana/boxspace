b913bffd132646eb0bcf0139b114bbcb1560654b
csantosmariana <56931876+csantosmariana@users.noreply.github.com>
2019-11-07 17:06:43 +0000

    Add files via upload

---
+++ parallax_effect.js
@@ -0,0 +1,34 @@
+$("#container").mousemove(function(e) {
+    parallaxIt(e, ".artwork1", -150);
+    
+});
+
+function parallaxIt(e, target, movement) {
+  var $this = $("#container");
+  var relX = e.pageX - $this.offset().left;
+  var relY = e.pageY - $this.offset().top;
+
+  TweenMax.to(target, 1, {
+    x: (relX - $this.width() / 2) / $this.width() * movement,
+    y: (relY - $this.height() / 2) / $this.height() * movement
+  });
+}
+
+
+jQuery(document).ready(function() {
+
+  var mouseX = 0, mouseY = 0;
+  var xp = 0, yp = 0;
+   
+  $(document).mousemove(function(e){
+    mouseX = e.pageX - 30;
+    mouseY = e.pageY - 30; 
+  });
+    
+  setInterval(function(){
+    xp += ((mouseX - xp)/6);
+    yp += ((mouseY - yp)/6);
+    $("#circle").css({left: xp +'px', top: yp +'px'});
+  }, 20);
+
+});
